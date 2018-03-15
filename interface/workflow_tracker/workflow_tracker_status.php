753f88263191b6b35b6b627356cdfe072af27b68
sloboy <30636948+sloboy@users.noreply.github.com>
2018-03-15 13:39:40 -0400

    removed the reference to workflow.inc

--- interface/workflow_tracker/workflow_tracker_status.php
+++ interface/workflow_tracker/workflow_tracker_status.php
@@ -19,7 +19,7 @@ require_once("../globals.php");
 require_once("$srcdir/options.inc.php");
 require_once("$srcdir/forms.inc");
 require_once("$srcdir/encounter_events.inc.php");
-require_once("$srcdir/workflow_tracker.inc.php");
+require_once("$srcdir/patient_tracker.inc.php");
 
 use OpenEMR\Core\Header;
 

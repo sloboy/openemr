6a0671c1e704dda5fb8e834cf20496e7d89c56b4
sloboy <30636948+sloboy@users.noreply.github.com>
2019-04-14 13:30:30 -0400

    added default patient type

--- interface/main/calendar/add_edit_event.php
+++ interface/main/calendar/add_edit_event.php
@@ -985,18 +985,34 @@ if (empty($collectthis)) {
     }
 
  // If we have a patient ID, get the name and phone numbers to display.
+ //dh-4/8/2018 added patient_type to SQL and set the $default_cat_id to the one in patient_data table
     if ($patientid) {
-        $prow = sqlQuery("SELECT lname, fname, phone_home, phone_biz, DOB " .
-         "FROM patient_data WHERE pid = ?", array($patientid));
-        $patientname = $prow['lname'] . ", " . $prow['fname'];
-        if ($prow['phone_home']) {
-            $patienttitle['phone_home'] = xl("Home Phone").": " . $prow['phone_home'];
+        $arow = sqlQuery("show columns from patient_data like 'patient_type';");
+        if ($arow) {
+            $prow = sqlQuery("SELECT lname, fname, phone_home, phone_biz, DOB, patient_type " .
+                "FROM patient_data WHERE pid = ?", array($patientid));
+            $arow = sqlQuery("Select pc_catid from openemr_postcalendar_categories where " .
+                "pc_constant_id = ?", array($prow['patient_type']));
+            $patientname = $prow['lname'] . ", " . $prow['fname'];
+            $default_catid = $arow['pc_catid'];
+            if ($prow['phone_home']) {
+                $patienttitle['phone_home'] = xl("Home Phone").": " . $prow['phone_home'];
+            }
+            if ($prow['phone_biz']) {
+                $patienttitle['phone_biz'] = xl("Work Phone").": " . $prow['phone_biz'];
+            }
+        }else {
+            echo('field DOES NOT exists');
+            
+            $prow = sqlQuery("SELECT lname, fname, phone_home, phone_biz, DOB " .
+            "FROM patient_data WHERE pid = ?", array($patientid));
+            $patientname = $prow['lname'] . ", " . $prow['fname'];
+            if ($prow['phone_home']) {
+                $patienttitle['phone_home'] = xl("Home Phone").": " . $prow['phone_home'];
+            }
         }
 
-        if ($prow['phone_biz']) {
-            $patienttitle['phone_biz'] = xl("Work Phone").": " . $prow['phone_biz'];
-        }
-    }
+   }
 
  // If we have a group id, get group data
     if ($groupid) {
